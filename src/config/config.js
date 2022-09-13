63f208ef3ebcd117ac07acca2290adc3004f34da
Yoda1221 <bfekete66@gmail.com>
2022-09-13 22:08:42 +0200

    Commit config

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,8 @@ export const ROLES = Object.freeze({
     admin: "Admin"
 })
 
+export const DASH_REGEX     = /^\/dash(\/)?$/
+export const USR_REGEX      = /^\/dash\/users(\/)?$/
 export const USER_REGEX     = /^[A-z0-9]{3,20}$/
 export const PWD_REGEX      = /^[A-z0-9!@#$%]{4,12}$/
 export const EMAIL_REGEX    = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
